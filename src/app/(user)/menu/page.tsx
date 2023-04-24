009ad0676ba118deb6a4dc4b300c0956da5218d4
Jaki <server8868@gmail.com>
2023-04-24 21:46:03 +0800

    Added layout and coloring admin/menu & profile

--- src/app/(user)/menu/page.tsx
+++ src/app/(user)/menu/page.tsx
@@ -1,7 +1,120 @@
 import { FC } from "react";
 
 const page: FC = ({}) => {
-    return <div>page</div>;
+    return (
+        <>
+        <div className="grid grid-cols-4 gap-4">
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://upload.wikimedia.org/wikipedia/commons/1/15/Cat_August_2010-4.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+            alt=""
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
+        </>
+    );
 };
 
 export default page;
