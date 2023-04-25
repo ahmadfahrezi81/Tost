b21ffd908890a4fffe4b4e2582ed7798f503c04f
Leonardo Laiherman <leonardolaiherman@MacBook-Air-Leonardo-6.local>
2023-04-25 23:23:42 +0800

    menuid, register, login, menu

---
+++ src/app/(admin)/admin/user/register/form.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import React, { useState } from "react"
+
+export const RegisterForm = () => {
+    const [email, setEmail] = useState ('')
+    const [password, setPassword] = useState ('')
+    const [error, setError] = useState<string | null>(null)
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+
+
+        console.log('Register!')
+    }
+
+    return(
+        <form onSubmit = {onSubmit} className="space-y-12 w-[400px] text-white">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            type="email"/>
+            </div>
+
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input 
+                className="w-full"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                id='password' 
+                type="password"/>
+            </div>
+            
+            {/* {error && <Alert>{error}</Alert>} */}
+            <div className="w-full">
+                <Button className="w-full" size="lg">
+                    Register
+                </Button>
+            </div>
+        </form>
+    )
+}
---
+++ src/app/(admin)/admin/user/register/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { RegisterForm } from "./form";
+
+export default function RegisterPage(){
+    return(
+        <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
+            <div className="shadow-xl px-8 pb-8 pt-12 bg-teal-800 rounded-xl space-y-12"> 
+                <h1 className="font-bold text-3xl text-center text-white">Create your Account</h1>
+                <RegisterForm />
+                <p className="text-center text-white">
+                    Have an account?  <Link className="text-indigo-400 hover:underline" href="/login">Sign in</Link>{' '} 
+                </p>
+            </div>
+        </div>
+    )
+}
